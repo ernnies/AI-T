82eaa66d37103bea95ab3c63a5aa44aa142ba89f
ernnies <ernestnnamdie@gmail.com>
2024-11-21 06:41:50 +0100

    sessionStore

--- app.js
+++ app.js
@@ -9,4 +9,6 @@ var http = require('http');
 var createError = require('http-errors');
 var bodyParser = require('body-parser');
 var passport = require('passport');
-var session = require()
+var session = require('express-session');
+var sessiontore = require('connect-sqlite3')(session);
+
